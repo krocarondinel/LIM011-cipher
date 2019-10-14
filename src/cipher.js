02fc853eac6ada00f75b26d39291f6c2d1d52064
krocarondinel <rocarondinel.kelly@gmail.com>
2019-10-13 23:27:47 -0500

    js, html, css listos

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,43 @@
 window.cipher = {
+  encode: (numberOfTrips, toTranslate) => {
+    let encryptedMessage = ''
+    for (let i = 0; i < toTranslate.length; i++) {
+      const ascii = toTranslate.charCodeAt(i)
+      // mayusculas, minusculas, números.
+      if (ascii >= 65 && ascii <= 90) {
+        const newAsciiCapitalLetters = (ascii - 65 + numberOfTrips) % 26 + 65
+        const newWordCapitalLetters = String.fromCharCode(newAsciiCapitalLetters)
+        encryptedMessage += newWordCapitalLetters
+      } else if (ascii >= 97 && ascii <= 122) {
+        const newAsciiLowercase = (ascii - 97 + numberOfTrips) % 26 + 97
+        const newWordLoweercase = String.fromCharCode(newAsciiLowercase)
+        encryptedMessage += newWordLoweercase
+      } else {
+        // casos especificos
+        encryptedMessage += toTranslate[i]
+      }
+    }
+    return encryptedMessage
+  },
 
+  decode: (numberOfTripsToBack, toDescipher) => {
+    let descipherMessage = ''
+    for (let i = 0; i < toDescipher.length; i++) {
+      const ascii2 = toDescipher.charCodeAt(i)
+      // mayusculas, minusculas, números
+      if (ascii2 >= 65 && ascii2 <= 90) {
+        const newAsciiCapitalLetters2 = (ascii2 - 90 - numberOfTripsToBack) % 26 + 90
+        const newWordCapitalLetters2 = String.fromCharCode(newAsciiCapitalLetters2)
+        descipherMessage += newWordCapitalLetters2
+      } else if (ascii2 >= 97 && ascii2 <= 122) {
+        const newAsciiLowercase2 = (ascii2 - 122 - numberOfTripsToBack) % 26 + 122
+        const newWordLoweercase2 = String.fromCharCode(newAsciiLowercase2)
+        descipherMessage += newWordLoweercase2
+      } else {
+        // casos especificos
+        descipherMessage += toDescipher[i]
+      }
+    }
+    return descipherMessage
+  }
 }
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,27 +1,25 @@
-global.window = global;
-require('../src/cipher');
+global.window = global
+require('../src/cipher')
 
 describe('cipher', () => {
   it('debería ser un object', () => {
-    expect(typeof cipher).toBe('object');
-  });
+    expect(typeof cipher).toBe('object')
+  })
   describe('cipher.encode', () => {
     it('debería ser una función', () => {
-      expect(typeof cipher.encode).toBe('function');
-    });
+      expect(typeof window.cipher.encode).toBe('function')
+    })
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
-      //escribe aquí tus test
-    });
+    // escribe aquí tus test
+      expect(window.cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG')
+    })
     // Hacker edition
-    //
     // [Español]
     // Si decides agregar soporte para minúsculas descomenta el test a
-    // continuación.
-    //
-    // it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
-    //   expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
-    // });
-
+    // continuación
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
+      expect(window.cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg')
+    })
     // Hacker edition
     //
     // [Español]
@@ -29,17 +27,17 @@ describe('cipher', () => {
     // el test a continuación.
     //
     //
-    // it('debería retornar " !@" para " !@"', () => {
-    //   expect(cipher.encode(33, ' !@')).toBe(' !@');
-    // });
+    it('debería retornar " !@" para " !@"', () => {
+      expect(window.cipher.encode(33, ' !@')).toBe(' !@')
+    })
   })
   describe('cipher.decode', () => {
     it('debería ser una función', () => {
-      expect(typeof cipher.decode).toBe('function');
-    });
+      expect(typeof window.cipher.decode).toBe('function')
+    })
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
-     //escribe aquí tus test
-    }); 
+      expect(window.cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    })
     //
     // Hacker edition
     //
@@ -48,10 +46,9 @@ describe('cipher', () => {
     // continuación.
     //
     //
-    // it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-    //   expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
-    // });
-
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+      expect(window.cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz')
+    })
     // Hacker edition
     //
     // [Español]
@@ -59,8 +56,8 @@ describe('cipher', () => {
     // el test a continuación.
     //
     //
-    // it('debería retornar " !@" para " !@"', () => {
-    //   expect(cipher.decode(33, ' !@')).toBe(' !@');
-    // });
-  });
-});
+    it('debería retornar " !@" para " !@"', () => {
+      expect(window.cipher.decode(33, ' !@')).toBe(' !@')
+    })
+  })
+})
